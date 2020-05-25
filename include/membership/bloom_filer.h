0bc8b014d550c8213f44e577e6483d6a5424b567
Hamed Zaghaghi <hamed.zaghaghi@gmail.com>
2020-05-25 16:34:50 +0430

    add docs for bloom filter

--- include/membership/bloom_filer.h
+++ include/membership/bloom_filer.h
@@ -6,10 +6,27 @@
 #include <bitset>
 #include <iostream>
 #include <memory>
+#include <string>
+#include <vector>
 
 #include "membership.h"
 
-template <std::size_t HC, std::size_t MC, template <typename...> class HF = MMH3HashFactory, typename T = std::string, typename S = uint32_t>
+/*
+ * Standard Bloom Filter
+ *
+ * BloomFilter class implements bloom filter algorithm for solving membership problem.
+ */
+template <
+    // Number of hash functions
+    std::size_t HC,
+    // Number of memory bits
+    std::size_t MC,
+    // Hash factory method class
+    template <typename...> class HF = MMH3HashFactory,
+    // Element type which will be inserted into bloom filter
+    typename T = std::string,
+    // Hash output size
+    typename S = uint32_t>
 class BloomFilter : public Membership<T> {
    private:
     std::unique_ptr<HF<T, S>> hash_factory_;
@@ -18,11 +35,35 @@ class BloomFilter : public Membership<T> {
     std::vector<std::unique_ptr<Hash<T, S>>> hashes_;
 
    public:
-    explicit BloomFilter();
-    ~BloomFilter();
+    // Default constructor
+    BloomFilter();
+
+    /* insert
+     * @item the item to insert into the bloom filter.
+     * 
+     * Insert item into bloom filter.
+     */
     void insert(const T& item) override;
+
+    /* erase
+     * @item the item to erase from filter.
+     * 
+     * Erase is not supported in standard bloom filter. will throw an exception
+     */
     void erase(const T& item) override;
+
+    /* clear
+     * 
+     * Clear filter and resets its internal memory.
+     */
     void clear() override;
+
+    /* contains
+     * @item the item to check for existence.
+     * 
+     * Check the item and report that it's in the filter or not.
+     * @return false if the item is not in the filter, true if item may be in the filter.
+     */
     bool contains(const T& item) const override;
 };
 
@@ -31,10 +72,6 @@ BloomFilter<HC, MC, HF, T, S>::BloomFilter() : hash_count_(HC) {
     hash_factory_ = std::make_unique<HF<T, S>>();
     bitset_memory_.reset();
     hashes_ = hash_factory_->createHashVector(HC);
-}
-
-template <std::size_t HC, std::size_t MC, template <typename...> class HF, typename T, typename S>
-BloomFilter<HC, MC, HF, T, S>::~BloomFilter() {
 }
 
 template <std::size_t HC, std::size_t MC, template <typename...> class HF, typename T, typename S>
@@ -67,4 +104,5 @@ bool BloomFilter<HC, MC, HF, T, S>::contains(const T& item) const {
     std::cout << bitset_memory_ << std::endl;
     return (bitset_memory_ & item_bitset) == item_bitset;
 }
+
 #endif   // INCLUDE_MEMBERSHIP_BLOOM_FILTER_H_
