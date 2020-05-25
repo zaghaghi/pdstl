adfa0533c3a197eb90b393f5e682cc6f3734ab3a
Hamed Zaghaghi <hamed.zaghaghi@gmail.com>
2020-05-25 15:50:43 +0430

    complete bloom filter

--- include/membership/bloom_filer.h
+++ include/membership/bloom_filer.h
@@ -1,17 +1,24 @@
 #ifndef INCLUDE_MEMBERSHIP_BLOOM_FILTER_H_
 #define INCLUDE_MEMBERSHIP_BLOOM_FILTER_H_
+#include <include/exception/not_supported.h>
+#include <include/hash/mmh3_hash_factory.h>
+
+#include <bitset>
+#include <iostream>
 #include <memory>
 
 #include "membership.h"
 
-template <typename T, typename HF>
+template <std::size_t HC, std::size_t MC, template <typename...> class HF = MMH3HashFactory, typename T = std::string, typename S = uint32_t>
 class BloomFilter : public Membership<T> {
    private:
-    std::unique_ptr<HF> hash_factory_;
-    uint16_t hash_count_;
+    std::unique_ptr<HF<T, S>> hash_factory_;
+    std::size_t hash_count_;
+    std::bitset<MC> bitset_memory_;
+    std::vector<std::unique_ptr<Hash<T, S>>> hashes_;
 
    public:
-    explicit BloomFilter(uint16_t hash_count);
+    explicit BloomFilter();
     ~BloomFilter();
     void insert(const T& item) override;
     void erase(const T& item) override;
@@ -19,29 +26,45 @@ class BloomFilter : public Membership<T> {
     bool contains(const T& item) const override;
 };
 
-template <typename T, typename HF>
-BloomFilter<T, HF>::BloomFilter(uint16_t hash_count) : hash_count_(hash_count) {
-    hash_factory_ = std::make_unique<HF>();
+template <std::size_t HC, std::size_t MC, template <typename...> class HF, typename T, typename S>
+BloomFilter<HC, MC, HF, T, S>::BloomFilter() : hash_count_(HC) {
+    hash_factory_ = std::make_unique<HF<T, S>>();
+    bitset_memory_.reset();
+    hashes_ = hash_factory_->createHashVector(HC);
 }
 
-template <typename T, typename HF>
-BloomFilter<T, HF>::~BloomFilter() {
+template <std::size_t HC, std::size_t MC, template <typename...> class HF, typename T, typename S>
+BloomFilter<HC, MC, HF, T, S>::~BloomFilter() {
 }
 
-template <typename T, typename HF>
-void BloomFilter<T, HF>::insert(const T& item) {
+template <std::size_t HC, std::size_t MC, template <typename...> class HF, typename T, typename S>
+void BloomFilter<HC, MC, HF, T, S>::insert(const T& item) {
+    std::cout << "insert(" << item << ")" << std::endl;
+    std::for_each(hashes_.cbegin(), hashes_.cend(), [this, &item](auto& hash) {
+        auto bit = hash->Value(item) % MC;
+        this->bitset_memory_.set(bit);
+    });
+    std::cout << bitset_memory_ << std::endl;
 }
 
-template <typename T, typename HF>
-void BloomFilter<T, HF>::erase(const T& item) {
+template <std::size_t HC, std::size_t MC, template <typename...> class HF, typename T, typename S>
+void BloomFilter<HC, MC, HF, T, S>::erase(const T& item) {
+    throw NotSupportedException();
 }
 
-template <typename T, typename HF>
-void BloomFilter<T, HF>::clear() {
+template <std::size_t HC, std::size_t MC, template <typename...> class HF, typename T, typename S>
+void BloomFilter<HC, MC, HF, T, S>::clear() {
+    bitset_memory_.reset();
 }
 
-template <typename T, typename HF>
-bool BloomFilter<T, HF>::contains(const T& item) const {
-    return false;
+template <std::size_t HC, std::size_t MC, template <typename...> class HF, typename T, typename S>
+bool BloomFilter<HC, MC, HF, T, S>::contains(const T& item) const {
+    std::bitset<MC> item_bitset;
+    std::for_each(hashes_.cbegin(), hashes_.cend(), [&item_bitset, &item](auto& hash) {
+        auto bit = hash->Value(item) % MC;
+        item_bitset.set(bit);
+    });
+    std::cout << bitset_memory_ << std::endl;
+    return (bitset_memory_ & item_bitset) == item_bitset;
 }
 #endif   // INCLUDE_MEMBERSHIP_BLOOM_FILTER_H_
